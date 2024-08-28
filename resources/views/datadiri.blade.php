444ac87f1fffdd3154feab0756281f5462d1a3b9
chikalnazmi <chikalnazmi05@gmail.com>
2024-08-28 14:37:23 +0700

    Data Diri

---
+++ resources/views/datadiri.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BIODATA DIRI</title>
+    </head>
+    <body>
+    <h1>BIODATA DIRI</h1><br>
+        <div class="content">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td>: Chikal Nazmi Mahira</td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>: 2241760122</td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>: SIB 3A</td>
+            </tr>
+            <tr>
+                <td>Prodi</td>
+                <td>: D4 Sistem Informsai Bisnis</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>: Teknologi Informasi</td>
+            </tr>
+            <tr>
+                <td>Tempat, Tgl Lahir</td>
+                <td>: Banjarmasin, 30 November 2003</td>
+            </tr>
+            <tr>
+                <td>Alamat </td>
+                <td>: Karangploso, Malang</td>
+            </tr>
+            <tr>
+                <td colspan=4 style="padding-bottom: 50px;"></td>
+            </tr>
+        </table>        
+        </div>
+    </body>
+</html>
