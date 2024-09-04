87e48cccd17ed894dd63827802862202d2a8f5cb
chikalnazmi <chikalnazmi05@gmail.com>
2024-09-04 10:21:17 +0700

    JS02-Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Jobsheet 1
+// Route::get('/', function () {
+//     return view('datadiri');
+
+// Jobsheet 2 Praktikum 1
+Route::get('/hello', function () 
+    { return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return 'Chikal Nazmi Mahira - 2241760122';
+});
+Route::get('/user/{Chikal}', function ($Chikal) {
+    return 'Nama saya '.$Chikal;
+    });
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID' .$id;
+});
+
+Route::get('/user/{name?}', function ($name='Chikal') {
+    return 'Nama saya '.$name;
+    });
+                               
+    
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +47,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('datadiri');
-});
+
