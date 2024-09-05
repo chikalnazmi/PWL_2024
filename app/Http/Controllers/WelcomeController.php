ce80a7ae5ffc184d5335a983f3142dcebccc342f
chikalnazmi <chikalnazmi05@gmail.com>
2024-09-05 08:35:46 +0700

    JS02-Praktikum3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,13 +9,17 @@ class WelcomeController extends Controller
     public function hello() {
         return 'Hello World';
         }
-    public function index() {
-        return 'Selamat Datang';
-        } 
-    public function about() {
-        return 'Chikal Nazmi Mahira - 2241760122';
-        } 
-    public function articles($id) {
-        return 'Halaman Artikel dengan ID' .$id;  
-        }         
+    public function greeting(){
+        return view('blog.hello', ['name' => 'Chikal']);
+    }
+
+    // public function index() {
+    //     return 'Selamat Datang';
+    //     } 
+    // public function about() {
+    //     return 'Chikal Nazmi Mahira - 2241760122';
+    //     } 
+    // public function articles($id) {
+    //     return 'Halaman Artikel dengan ID' .$id;  
+    //     }         
 }
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,57 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 
-use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\PhotoController;        
+
+
+// Jobsheet 1
+Route::get('/', function () {
+    return view('datadiri');
+
+// Jobsheet 2 Praktikum 1   
+Route::get('/hello', function () 
+    { return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return 'Chikal Nazmi Mahira - 2241760122';
+});
+
+Route::get('/user/{Chikal}', function ($Chikal) {
+    return 'Nama saya '.$Chikal;
+    });
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID' .$id;
+});
+
+Route::get('/user/{name?}', function ($name='Chikal') {
+    return 'Nama saya '.$name;
+    });
+                        
+// Jobsheet 2 Praktikum 2
+Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/index', [WelcomeController::class,'index']);
+Route::get('/about', [WelcomeController::class,'about']);
+Route::get('/articles/{id}', [WelcomeController::class,'articles']);
+
+Route::get('/index', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
 
 Route::resource('photos', PhotoController::class);
-
 Route::resource('photos', PhotoController::class)->only([
     'index', 'show'
     ]);
@@ -20,47 +61,12 @@ Route::resource('photos', PhotoController::class)->except([
     ]);
 
 
-// Jobsheet 1
-// Route::get('/', function () {
-//     return view('datadiri');
+// Jobsheet 2 Praktikum 3
+Route::get('/greeting', function () {
+    return view('blog.hello', ['name' => 'Chikal']);
+});
 
-// Jobsheet 2 Praktikum 1   
-// Route::get('/hello', function () 
-//     { return 'Hello World';
-// });
-// Route::get('/world', function () {
-//     return 'World';
-// });
-// Route::get('/welcome', function () {
-//     return 'Selamat Datang';
-// });
-// Route::get('/about', function () {
-//     return 'Chikal Nazmi Mahira - 2241760122';
-// });
-// Route::get('/user/{Chikal}', function ($Chikal) {
-//     return 'Nama saya '.$Chikal;
-//     });
-// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-// });
-// Route::get('/articles/{id}', function ($id) {
-//     return 'Halaman Artikel dengan ID' .$id;
-// });
-
-// Route::get('/user/{name?}', function ($name='Chikal') {
-//     return 'Nama saya '.$name;
-//     });
-                        
-// Jobsheet 2 Praktikum 2
-// Route::get('/hello', [WelcomeController::class,'hello']);
-// Route::get('/index', [WelcomeController::class,'index']);
-// Route::get('/about', [WelcomeController::class,'about']);
-// Route::get('/articles/{id}', [WelcomeController::class,'articles']);
-
-// Route::get('/index', [HomeController::class,'index']);
-// Route::get('/about', [AboutController::class,'about']);
-// Route::get('/articles/{id}', [ArticleController::class,'articles']);
-
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
 
 
 
